b05cfffe8c8574acd7cc3293775bdd4bed4cb4aa
AvoidKun <145783473+AvoidKun@users.noreply.github.com>
2025-01-11 22:41:45 +0800

    {Avoid}
    项目的初始化

---
+++ src/main/java/com/avoid/phbs/PhbsApplication.java
@@ -0,0 +1,13 @@
+package com.avoid.phbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avoid/phbs/PhbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avoid.phbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
